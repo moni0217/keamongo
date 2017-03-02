d87fdbff830905bcbdfebeaab7230aed76e04e9c
moni0217 <moni0217@stud.kea.dk>
2017-03-02 09:31:03 +0100

    Added markers and a new center variable

--- script.js
+++ script.js
@@ -1,13 +1,43 @@
 var map;
+var center = {
+    lat: 50.075760,
+    lng: 8.606346
+}
 var lygten = {
     lat: 55.706398,
     lng: 12.539141
+};
+var lyon = {
+    lat: 45.774535,
+    lng: 4.848811
+};
+var alcudia = {
+    lat: 39.840828,
+    lng: 3.127730
 };
 
 
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
-        center: lygten,
-        zoom: 14
+        center: center,
+        zoom: 5
     });
+
+    var marker = new google.maps.Marker({
+        position: lyon,
+        map: map,
+        title: 'Lyon!'
+    })
+
+    var marker = new google.maps.Marker({
+        position: lygten,
+        map: map,
+        title: 'Lygten!'
+    })
+
+    var marker = new google.maps.Marker({
+        position: alcudia,
+        map: map,
+        title: 'Alcudia!'
+    })
 }
