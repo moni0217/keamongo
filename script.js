8c2fb2639bd94170d808c9a649dc05ecee896d59
moni0217 <moni0217@stud.kea.dk>
2017-03-02 09:19:00 +0100

    Made var = lygten in script. Enables me to have multiple points on the map.

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-  var map;
+var map;
+var lygten = {
+    lat: 55.706398,
+    lng: 12.539141
+};
 
-  function initMap() {
-      map = new google.maps.Map(document.getElementById('map'), {
-          center: {
-              lat: 55.706398,
-              lng: 12.539141
-          },
-          zoom: 14
-      });
-  }
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: lygten,
+        zoom: 14
+    });
+}
